f9a3adb9a5eb57b90dc8bc1008c97c1b13460260
AJIMI MAHDI <AJIMIMAHDI1@GMAIL.COM>
2025-12-10 20:21:50 +0100

    agggiunto revers per invertire l'ordine e include se fabio e presente

--- script.js
+++ script.js
@@ -10,19 +10,19 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+const reversedTeachers = teachers.reverse();
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 const longNames = teachers.filter( teachers => teachers.length >= 5);
 
 // 3. Rimuovi 'Ed' dall'array teachers
+//da chiedere a riccardo se si puo usare filter
 
 ;
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-
-
+const isFabioPresent = teachers.includes('Fabio');
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 
